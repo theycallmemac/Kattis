5c95a04dc1c83a0ed3f5d9a03347b975f1911b0c
James Mc Dermott <james.mcdermott89@gmail.com>
2017-05-12 11:28:18 +0100

    Update carrots.py

--- Python/carrots.py
+++ Python/carrots.py
@@ -1,2 +1,6 @@
-contestants, problems = (int(i) for i in input().split())
-print(problems)
+def main():
+  contestants, problems = (int(i) for i in input().split())
+  print(problems)
+if __name__ == '__main__':
+	main()
+  
