5a534c4ac3ebd2d031cd6e2459207e0646545635
James Mc Dermott <james.mcdermott89@gmail.com>
2017-06-10 14:30:01 +0100

    Create kornislav.java

---
+++ Java/kornislav.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Kattis {
+
+    public static void main(String[] args) {
+
+    	 List<Integer> stepsTaken = new ArrayList<Integer>();
+
+        Scanner in = new Scanner(System.in);
+
+		stepsTaken.add(in.nextInt());
+		stepsTaken.add(in.nextInt());
+		stepsTaken.add(in.nextInt());
+		stepsTaken.add(in.nextInt());
+
+		Collections.sort(stepsTaken);
+		
+		System.out.println(stepsTaken.get(0) * stepsTaken.get(2));
+
+
+    }
+}
