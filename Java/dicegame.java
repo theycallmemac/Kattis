0e169921fb499ab09125754401e823f153246733
James Mc Dermott <james.mcdermott89@gmail.com>
2017-06-10 14:14:53 +0100

    Create dicegame.java

---
+++ Java/dicegame.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Kattis {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        float gunnar = ( (in.nextFloat() + in.nextFloat() ) / 2) + ( (in.nextFloat() + in.nextFloat() ) / 2);
+        float emma = ( (in.nextFloat() + in.nextFloat() ) / 2) + ( (in.nextFloat() + in.nextFloat() ) / 2);
+
+        if(gunnar > emma){
+            System.out.println("Gunnar");
+        }
+
+        else if(gunnar < emma){
+            System.out.println("Emma");
+        }
+        
+        else{
+			      System.out.println("Tie");
+		    }
+
+    }
+    
+}
