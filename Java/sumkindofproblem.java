e58c1d316446d06b1d99492ce24756d0fd973681
James Mc Dermott <james.mcdermott89@gmail.com>
2017-06-13 14:46:51 +0100

    Create sumkindofproblem.java

---
+++ Java/sumkindofproblem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Kattis {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int p = in.nextInt();
+
+        for(int i = 0; i < p; i++){
+
+        	int k = in.nextInt();
+            System.out.print(k + " ");
+
+            int n = in.nextInt();
+            System.out.print((n * (n + 1) / 2) + " ");
+            System.out.print(n * n + " ");
+            System.out.print(n * (n + 1));
+            System.out.println();
+            
+        }
+        
+    }
+
+}
